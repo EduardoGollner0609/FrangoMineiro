b3dda7a9293746954c259b6e5eba5b673356bfcc
EduGollner0609 <dudugollner@gmail.com>
2024-12-19 01:27:34 -0300

    Criando HomePage.

---
+++ FrontEnd/src/routes/ClientPage/HomePage/index.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <section id="section-home-page">
+            <header>
+                <h2>Farofas</h2>
+            </header>
+        </section>
+    );
+}
