2f72d306a0f7a36449322658c91df705f82981ac
EduGollner0609 <dudugollner@gmail.com>
2024-12-19 02:07:47 -0300

    Ajeitando Header, estilizando e adicionando elementos.

--- FrontEnd/src/routes/ClientPage/HomePage/index.tsx
+++ FrontEnd/src/routes/ClientPage/HomePage/index.tsx
@@ -1,11 +1,31 @@
 import SearchBar from "../../../components/SearchBar";
+import messageIcon from '../../../assets/message-icon.svg';
+import notificationIcon from '../../../assets/notification-icon.svg';
+import userImg from '../../../assets/user-img.svg';
 
 export default function HomePage() {
     return (
         <section id="section-home-page">
-            <header>
+            <header className="header-home-page">
                 <h2>Farofas</h2>
                 <SearchBar />
+                <div className="home-page-user-details">
+                    <div className="home-page-user-details-item">
+                        <a href="">
+                            <img src={messageIcon} alt="" />
+                        </a>
+                    </div>
+                    <div className="home-page-user-details-item">
+                        <a href="">
+                            <img src={notificationIcon} alt="" />
+                        </a>
+                    </div>
+                    <div className="home-page-user-details-item">
+                        <a href="">
+                            <img src={userImg} alt="" />
+                        </a>
+                    </div>
+                </div>
             </header>
         </section>
     );
